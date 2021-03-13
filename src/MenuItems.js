251b10e991d737449b5febf7bf8beb4eb8ac443c
Harsh Verma <harshverma0362@gmail.com>
2021-03-13 22:18:46 +0530

    menu items home screen implimented

---
+++ src/MenuItems.js
@@ -0,0 +1,40 @@
+const menuItems = [
+  {
+    _id: 1,
+    name: "Burgar",
+    price: "59.00",
+    image: "/images/burgar.jpg",
+  },
+  {
+    _id: 2,
+    name: "Choclate Cake",
+    price: "159.00",
+    image: "/images/choclate_cake.jpg",
+  },
+  {
+    _id: 3,
+    name: "French Fries",
+    price: "49.00",
+    image: "/images/fries.jpg",
+  },
+  {
+    _id: 4,
+    name: "Salad",
+    price: "29.00",
+    image: "/images/fruits.jpg",
+  },
+  {
+    _id: 5,
+    name: "Nodles",
+    price: "89.00",
+    image: "/images/nodles.jpg",
+  },
+  {
+    _id: 6,
+    name: "Rool",
+    price: "99.00",
+    image: "/images/rool.jpg",
+  },
+];
+
+export default menuItems;
--- src/Screens/MenuItemsScreen.js
+++ src/Screens/MenuItemsScreen.js
@@ -1,32 +1,46 @@
 import React from "react";
-import { Box } from "@material-ui/core";
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+import menuItems from "../MenuItems";
 
 const MenuItemsScreen = () => {
   return (
-    <Box height="100vh" overflow="scroll" pt={6}>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit cumque
-        maiores commodi doloribus blanditiis, placeat, dolores error, ad eaque
-        temporibus tempore reprehenderit corrupti deleniti. Dolore natus
-        aspernatur magnam illum, repellendus dolorem libero earum cumque
-        temporibus voluptatem incidunt! Deleniti vitae quod, sit odio a expedita
-        dolore magnam voluptatum nihil maxime impedit ipsum pariatur explicabo
-        tempora minima aut consequuntur eum, laboriosam aliquam? Architecto
-        error numquam obcaecati quas voluptatibus, ut velit officiis ex id modi,
-        ipsa iusto quia ab molestiae tenetur voluptates possimus aliquam? Cumque
-        assumenda adipisci eius, amet facilis, dolore reiciendis aperiam
-        doloremque eum a rem illum dolorem. Numquam eos debitis neque. cumque
-        maiores commodi doloribus blanditiis, placeat, dolores error, ad eaque
-        temporibus tempore reprehenderit corrupti deleniti. Dolore natus
-        aspernatur magnam illum, repellendus dolorem libero earum cumque
-        temporibus voluptatem incidunt! Deleniti vitae quod, sit odio a expedita
-        dolore magnam voluptatum nihil maxime impedit ipsum pariatur explicabo
-        tempora minima aut consequuntur eum, laboriosam aliquam? Architecto
-        error numquam obcaecati quas voluptatibus, ut velit officiis ex id modi,
-        ipsa iusto quia ab molestiae tenetur voluptates possimus aliquam? Cumque
-        assumenda adipisci eius, amet facilis, dolore reiciendis aperiam
-        doloremque eum a rem illum dolorem. Numquam eos debitis neque.
-      </p>
+    <Box height="100vh" overflow="scroll" pt={8} pr={1}>
+      <Grid container spacing={2}>
+        {menuItems.map((item) => (
+          <Grid item key={item._id}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  image={item.image}
+                  alt={item.name}
+                  title={item.name}
+                  height={120}
+                />
+                <CardContent
+                  component={Box}
+                  style={{ padding: 3, paddingLeft: 6, paddingRight: 6 }}
+                  display="flex"
+                  flexDirection="row"
+                >
+                  <Typography>{item.name}</Typography>
+                  <Typography style={{ marginLeft: "auto" }}>
+                    Rs. {item.price}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </Box>
   );
 };
